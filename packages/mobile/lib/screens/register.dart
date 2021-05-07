8049207347a81848f5abb2734ee05a1e5e546b5d
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2021-05-07 17:43:16 +0800

    Add initial files

---
+++ packages/mobile/lib/screens/register.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      child: TextField(),
+    ));
+  }
+}
